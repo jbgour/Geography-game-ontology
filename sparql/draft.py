6dc329165895bfb8983792326588ca7dfd763d8a
jbgour <jeanbaptiste.gourlet@hotmail.fr>
2021-01-31 17:49:20 +0100

    initializing repo

---
+++ sparql/draft.py
@@ -0,0 +1,28 @@
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+sparql.setQuery("""
+    PREFIX dbo: <http://dbpedia.org/ontology/> 
+    PREFIX foaf: <http://xmlns.com/foaf/0.1/> 
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+
+    select distinct ?countrylabel
+    where { 
+        ?country a <http://dbpedia.org/class/yago/WikicatMemberStatesOfTheUnitedNations> .
+        ?country rdfs:label ?countrylabel .
+        FILTER ( lang(?countrylabel) = 'fr')
+}
+
+""")
+
+sparql.setReturnFormat(JSON)
+results = sparql.query().convert()
+
+print(results)
+
+sparql.setQuery("""
+    SELECT DISTINCT ?placeName WHERE {
+     ?placeName a yago:City108524735 ; dbo:country dbr:India
+    }
+
+""")
